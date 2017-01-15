75d33a5e011c096800ae23173fad05a5b18733cd
John Currie <johntcurrie@gmail.com>
2017-01-15 15:44:24 -0800

    Refactored index.js to use ORM

--- server/db/index.js
+++ server/db/index.js
@@ -5,30 +5,19 @@ var User = orm.define('User', {
 	username: Sequelize.STRING
 });
 
-var Message = orm.define('Message', {
 
+var Message = orm.define('Message', {
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
 });
+ 
+Message.belongsTo(User);
+User.hasMany(Message);
+
+User.sync();
+Message.sync();
+
+exports.Message = Message;
+exports.User = User;
 
 var mysql = require('mysql');
-
-// Create a database connection and export it from this file.
-// You will need to connect with the ssers "root", no password,
-// and to the database "chat".
-
-
-exports.connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'chat'
-});
-
-exports.connection.connect();
-
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//   if (err) {
-//     throw err;
-//   }
-
-//   console.log('The solution is: ', rows[0].solution);
-// });
